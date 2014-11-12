eff0793bee51feb338a3fef1c91e58497401500d
Alex Sjoman <alex.sjoman@datasift.com>
2014-11-12 15:35:38 -0800

    use API v1.1 to get all subs

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template,request,session
 from datasift import Client
+from datasift.push import Push
+from datasift.request import PartialRequest, DatasiftAuth
 
 app = Flask(__name__)
 
@@ -36,15 +38,20 @@ def log_the_user_in():
     client = Client(session['username'],session['apikey'])
     pushget = client.push.get(include_finished=True)
     pushgetstr = [ str(p) for p in pushget['subscriptions']]
-    return render_template('console.html', response=pushgetstr)
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+    # custom push get request, so we can pass all to API v.1.1 
+    # should only be for internal accounts
+    request = PartialRequest(DatasiftAuth(session['username'],session['apikey']),prefix='push')
+    params = {'include_finished':1,'all':1,'per_page':100}
+    pushget = request.get('get',params)
+    pushgetstr = [ str(p) for p in pushget['subscriptions']]
 
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+    historicget = client.historics.get()
+    historicget = client.historics.get(maximum=historicget['count'])
+    historicgetstr = [str(h) for h in historicget['data']]
+    return render_template('console.html', 
+        name=session['username'],push=pushgetstr, historic=historicgetstr)
+
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
